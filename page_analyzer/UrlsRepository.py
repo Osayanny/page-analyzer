f44787cc4e35d1cc0b1a79d263c234ed7c14fcf0
Osayanny <osayanni7@gmail.com>
2024-11-19 15:22:32 +0500

    create urls repository

---
+++ page_analyzer/UrlsRepository.py
@@ -0,0 +1,53 @@
+import os
+import psycopg2 as pg2
+from psycopg2.extras import DictCursor
+from dotenv import load_dotenv
+
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+connect = pg2.connect(DATABASE_URL)
+
+
+class Urls:
+    def __init__(self, conn=connect):
+        self.conn = conn
+
+    def get_content(self):
+        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("SELECT * FROM urls ORDER BY id DESC")
+            return [dict(row) for row in cur]
+
+    def find(self, id):
+        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("SELECT * FROM urls WHERE id=%s", (id,))
+            row = cur.fetchone()
+            return dict(row) if row else None
+
+    def _find_by_name(self, name):
+        with self.conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute("SELECT * FROM urls WHERE name ILIKE %s", (name, ))
+            return cur.fetchall()
+
+    def _create(self, url):
+        with self.conn.cursor() as cur:
+            cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id", (url['name'], url['created_at']))
+            id = cur.fetchone()[0]
+            url['id'] = id
+        self.conn.commit()
+        return url
+
+    def save(self, url):
+        urls = self._find_by_name(url['name'])
+        if not urls:
+            return (self._create(url), 'success')
+        else: 
+            return (urls[0], 'exist')
+            
+
+
+            
+
+
+
+        
