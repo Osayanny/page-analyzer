dbe588caee7a9bebd941d7cce822c46a44866945
Osayanny <osayanni7@gmail.com>
2024-11-23 14:13:25 +0500

    fix connections

--- page_analyzer/UrlsRepository.py
+++ page_analyzer/UrlsRepository.py
@@ -6,30 +6,37 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
-connect = pg2.connect(DATABASE_URL)
-connect.autocommit = True
+
+def _get_connection():
+    connect = pg2.connect(DATABASE_URL)
+    return connect
 
 
 class Urls:
-    def __init__(self, conn=connect):
-        self.conn = conn
+    def __init__(self):
+        self.conn = _get_connection()
 
     def get_content(self):
         with self.conn.cursor(cursor_factory=DictCursor) as cur:
             cur.execute("SELECT * FROM urls ORDER BY id DESC")
             rows = cur.fetchall()
+        self.conn.close()
+
         return [dict(row) for row in rows]
 
     def find(self, id):
         with self.conn.cursor(cursor_factory=DictCursor) as cur:
             cur.execute("SELECT * FROM urls WHERE id=%s", (id,))
             row = cur.fetchone()
+
+        self.conn.close()
         return dict(row) if row else None
 
     def _find_by_name(self, name):
         with self.conn.cursor(cursor_factory=DictCursor) as cur:
             cur.execute("SELECT * FROM urls WHERE name ILIKE %s", (name, ))
             res = cur.fetchall()
+        self.conn.close()
         return res
 
     def _create(self, url):
@@ -39,6 +46,8 @@ class Urls:
                 (url['name'], url['created_at']))
             id = cur.fetchone()[0]
             url['id'] = id
+        self.conn.commit()
+        self.conn.close()
         return url
 
     def save(self, url):
