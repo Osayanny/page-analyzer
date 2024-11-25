1900ded2e1fef850d6f895c3c25ab2bb3b4e129d
Osayanny <osayanni7@gmail.com>
2024-11-25 13:32:50 +0500

    Create table for checks

--- database.sql
+++ database.sql
@@ -1,7 +1,18 @@
 drop table if exists urls;
+dop table if exists url_checks;
 
-create table Urls (
+create table urls (
 id int primary key generated always as identity,
 name varchar(255) unique not null,
 created_at date not null
+);
+
+create table url_checks (
+    id int primary key generated always as identity,
+    url_id int references urls (id) not null,
+    status_code int,
+    h1 text,
+    title text,
+    description text,
+    created_at date not null
 );
