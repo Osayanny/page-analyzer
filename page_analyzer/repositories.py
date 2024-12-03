7c2c390242873d0732658bf7018c078a777ebded
Osayanny <osayanni7@gmail.com>
2024-12-03 15:44:38 +0500

    refactoring

--- page_analyzer/repositories.py
+++ page_analyzer/repositories.py
@@ -17,36 +17,47 @@ class Urls:
     def __init__(self):
         self.conn = _get_connection()
 
-    def get_content(self):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT * FROM urls ORDER BY id DESC")
-            rows = cur.fetchall()
-        self.conn.close()
+    def execute_query(self, query, parms=None, factory=DictCursor):
+        with self.conn.cursor(cursor_factory=factory) as cur:
+            cur.execute(query, parms)
+            if query.lstrip().startswith('SELECT'):
+                return cur.fetchall()
+            else:
+                return cur.fetchone()
 
+
+    def get_content(self):
+        query = "SELECT * FROM urls ORDER BY id DESC"
+        rows = self.execute_query(query)
+
+        self.conn.close()
         return [dict(row) for row in rows]
 
     def find(self, id):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT * FROM urls WHERE id=%s", (id,))
-            row = cur.fetchone()
+        query = "SELECT * FROM urls WHERE id=%s"
+        parms = (id,)
+        row = self.execute_query(query, parms)
 
         self.conn.close()
-        return dict(row) if row else None
+        return dict(row[0]) if row else None
 
     def _find_by_name(self, name):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT * FROM urls WHERE name ILIKE %s", (name, ))
-            res = cur.fetchall()
+        query = "SELECT * FROM urls WHERE name ILIKE %s"
+        parms = (name, )
 
+        res = self.execute_query(query, parms)
         return res
 
     def _create(self, url):
-        with self.conn.cursor() as cur:
-            cur.execute(
-                "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id", # noqa
-                (url['name'], url['created_at']))
-            id = cur.fetchone()[0]
-            url['id'] = id
+        query = """
+            NSERT INTO urls (name, created_at)
+            VALUES (%s, %s) RETURNING id
+            """
+        parms = (url['name'], url['created_at'])
+
+        id = self.execute_query(query,parms, factory=None)[0]
+        url['id'] = id
+
         self.conn.commit()
         self.conn.close()
         return url
@@ -64,64 +75,68 @@ class Checks:
     def __init__(self):
         self.conn = _get_connection()
 
+    def execute_query(self, query, parms=None, factory=DictCursor):
+        with self.conn.cursor(cursor_factory=factory) as cur:
+            cur.execute(query, parms)
+            if query.lstrip().startswith('SELECT'):
+                return cur.fetchall()
+            else:
+                return cur.fetchone()
+
     def get_checks(self, id):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute(
-                """
-                SELECT *
-                FROM url_checks
-                WHERE url_id=%s
-                ORDER BY id DESC
-                """, (id,))
-            rows = cur.fetchall()
+        query = """
+            SELECT *
+            FROM url_checks
+            WHERE url_id=%s
+            ORDER BY id DESC
+            """
+        parms = (id,)
+        rows = self.execute_query(query, parms)
 
         self.conn.close()
         return [dict(row) for row in rows]
 
     def get_url_with_last_check(self):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("""
-                SELECT
-                    url.id,
-                    url.name,
-                    MAX(checks.created_at) AS last_check,
-                    checks.status_code
-                FROM urls AS url
-                LEFT JOIN url_checks AS checks
-                ON url.id = checks.url_id
-                GROUP BY url.id, url.name, checks.status_code
-                ORDER BY url.id DESC
-                """)
-            rows = cur.fetchall()
+        query = """
+            SELECT
+                url.id,
+                url.name,
+                MAX(checks.created_at) AS last_check,
+                checks.status_code
+            FROM urls AS url
+            LEFT JOIN url_checks AS checks
+            ON url.id = checks.url_id
+            GROUP BY url.id, url.name, checks.status_code
+            ORDER BY url.id DESC
+            """
+        rows = self.execute_query(query)
 
         self.conn.close()
         return [dict(row) for row in rows]
 
     def save(self, check):
-        with self.conn.cursor() as cur:
-            cur.execute(
-                """INSERT INTO
-                    url_checks (
-                        url_id,
-                        status_code,
-                        h1,
-                        title,
-                        description,
-                        created_at
-                    )
-                    VALUES (%s, %s, %s, %s, %s, %s)
-                    RETURNING id""",
-                (
-                    check['url_id'],
-                    check['code'],
-                    check['h1'],
-                    check['title'],
-                    check['description'],
-                    check['created_at']
-                    )
-            )
-            id = cur.fetchone()[0]
-            check['id'] = id
+        query = """
+            INSERT INTO url_checks (
+                    url_id,
+                    status_code,
+                    h1,
+                    title,
+                    description,
+                    created_at
+                )
+            VALUES (%s, %s, %s, %s, %s, %s)
+            RETURNING id"""
+        parms = (
+            check['url_id'],
+            check['code'],
+            check['h1'],
+            check['title'],
+            check['description'],
+            check['created_at']
+        )
+            
+        id = self.execute_query(query, parms, factory=None)[0]
+        check['id'] = id
 
         self.conn.commit()
         self.conn.close()
